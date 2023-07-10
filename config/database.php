8c63f4fe56c322d0919da170e1ed8214c79868d0
dihjital <peter.hrobar@gmail.com>
2023-07-10 15:22:25 +0200

    Bug fixes and performance improvements

--- config/database.php
+++ config/database.php
@@ -16,6 +16,7 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'sqlite'),
+    'database' => database_path('database.sqlite'),
 
     /*
     |--------------------------------------------------------------------------
