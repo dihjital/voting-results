15468bff40f6700e244cc7c8cd7172c12678fd4b
Peter Hrobar <peter.hrobar@gmail.com>
2023-07-11 10:49:20 +0200

    Bug fixes and performance improvements

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Auth::loginUsingId(1, $remember = true);
+// Auth::loginUsingId(1, $remember = true);
 
 Route::get('/', function () {
     // return view('welcome');
