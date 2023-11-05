11978133512cb99e2fe69ea460dccd9509bd82dc
dihjital <peter.hrobar@gmail.com>
2023-11-05 16:36:14 +0100

    Embed chart image in vote results

--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,8 +1,21 @@
 @component('mail::message')
 {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
-<img src="https://quickchart.io/chart/render/zm-a675233a-1e7e-4d96-8915-0a65c79b533a
-    &labels={{ implode(',', array_map(fn($r) => urlencode($r['vote_text']), $voteResults)) }}
+<img src="https://quickchart.io/chart/render/zm-63e662c8-2be4-4426-9105-cbcd414ea1af?title=
+    {{ urlencode($questionText) }}
+    &labels={{ 
+        implode(',', 
+            array_map(
+                fn($r) => 
+                    urlencode(
+                        strlen($r['vote_text']) > 10 
+                            ? substr($r['vote_text'], 0, 10) . '...' 
+                            : $r['vote_text']
+                    ),
+                $voteResults
+            )
+        )
+    }}
     &data1={{ implode(',', array_map(fn($r) => $r['number_of_votes'], $voteResults)) }}"
 />
 
