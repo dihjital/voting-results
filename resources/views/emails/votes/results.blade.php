27924d29d3ab04220da17be03ae97a28582e43d9
dihjital <peter.hrobar@gmail.com>
2023-11-05 12:44:33 +0100

    Bug fixes and performance improvements

--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,16 +1,15 @@
 @component('mail::message')
 {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
-@component('mail::panel')
 <img src="data:image/png; base64,{{ 
     base64_encode(
     file_get_contents('https://quickchart.io/chart/render/zm-63e662c8-2be4-4426-9105-cbcd414ea1af?title=' . 
     urlencode('Mi volt Péter jele az oviban') . 
     '?&labels=' . 
     urlencode('Pöttyös labda,Talicska,vagy fésű?') . 
-    '&data1=8,4,7')) }}" 
+    '&data1=8,4,7')) }}"
+    width="640" height="480"
     />
-@endcomponent
 
 @component('mail::table')
 | #     | VOTE TEXT | # OF VOTES |
