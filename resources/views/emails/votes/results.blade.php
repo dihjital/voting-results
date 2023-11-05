ba36eb3a660ca3fca124d5469c92f7199211c232
dihjital <peter.hrobar@gmail.com>
2023-11-05 12:39:43 +0100

    Add chart image to results e-mail

--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,6 +1,17 @@
 @component('mail::message')
 {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
+@component('mail::panel')
+<img src="data:image/png; base64,{{ 
+    base64_encode(
+    file_get_contents('https://quickchart.io/chart/render/zm-63e662c8-2be4-4426-9105-cbcd414ea1af?title=' . 
+    urlencode('Mi volt Péter jele az oviban') . 
+    '?&labels=' . 
+    urlencode('Pöttyös labda,Talicska,vagy fésű?') . 
+    '&data1=8,4,7')) }}" 
+    />
+@endcomponent
+
 @component('mail::table')
 | #     | VOTE TEXT | # OF VOTES |
 | :---- | :-------- | :--------: |
@@ -9,7 +20,7 @@
 @endforeach
 @endcomponent
 
-{{ __('Is this gets modified at all? If you would like to check out the voting results on the Web site please click on the button below:') }}
+{{ __('If you would like to check out the voting results on the Web site please click on the button below:') }}
 
 @component('mail::button', ['url' => $resultsUrl])
 {{ __('Check out the results') }}
