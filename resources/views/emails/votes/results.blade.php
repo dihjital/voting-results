dc0017661591d3edbb278f3afdde702684045b37
dihjital <peter.hrobar@gmail.com>
2023-11-05 11:52:29 +0100

    Add chart to voting results e-mail

--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,15 +1,7 @@
 @component('mail::message')
 {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
-{{-- @component('mail::panel')
-<img src="https://quickchart.io/chart/render/zm-4bab0620-25fd-4265-a098-ca366fc2c2e2?title=
-         {{ $questionText }}
-         ?&labels={{ implode(',', array_map(fn($r) => $r['vote_text'], $voteResults)) }}
-         &data1={{ implode(',', array_map(fn($r) => $r['number_of_votes'], $voteResults)) }}"
-/>
-@endcomponent --}}
-
-<img src="https://quickchart.io/chart/render/zm-4bab0620-25fd-4265-a098-ca366fc2c2e2?title=Mi volt Péter jele az oviban?&labels=Pöttyös labda,Talicska,vagy fésű?&data1=8,4,7" />
+![Voting result chart](https://quickchart.io/chart/render/zm-63e662c8-2be4-4426-9105-cbcd414ea1af?title=Mi volt Péter jele az oviban?&labels=Pöttyös labda,Talicska,vagy fésű?&data1=8,4,7 "Voting results chart")
 
 @component('mail::table')
 | #     | VOTE TEXT | # OF VOTES |
