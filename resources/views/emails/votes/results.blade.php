2266e9ebcc65437eda95e02c72c6aa5d76bc8926
dihjital <peter.hrobar@gmail.com>
2023-11-05 11:36:57 +0100

    Try with a fixes image

--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,13 +1,15 @@
 @component('mail::message')
 {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
-@component('mail::panel')
+{{-- @component('mail::panel')
 <img src="https://quickchart.io/chart/render/zm-4bab0620-25fd-4265-a098-ca366fc2c2e2?title=
          {{ $questionText }}
          ?&labels={{ implode(',', array_map(fn($r) => $r['vote_text'], $voteResults)) }}
          &data1={{ implode(',', array_map(fn($r) => $r['number_of_votes'], $voteResults)) }}"
 />
-@endcomponent
+@endcomponent --}}
+
+<img src="https://quickchart.io/chart/render/zm-4bab0620-25fd-4265-a098-ca366fc2c2e2?title=Mi volt Péter jele az oviban?&labels=Pöttyös labda,Talicska,vagy fésű?&data1=8,4,7" />
 
 @component('mail::table')
 | #     | VOTE TEXT | # OF VOTES |
