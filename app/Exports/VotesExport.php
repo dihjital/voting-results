a1d976a1edf82ccb58a14e0f380896bbbaba3949
dihjital <peter.hrobar@gmail.com>
2023-11-05 11:18:12 +0100

    Add chart to e-mail

--- app/Exports/VotesExport.php
+++ app/Exports/VotesExport.php
@@ -16,11 +16,15 @@ class VotesExport implements FromArray, WithHeadings
 
     public function array(): array
     {
-        return array_map(fn($vote) => [
-                $vote['id'],
-                $vote['vote_text'],
-                $vote['number_of_votes'] ?: '0', // No votes received yet
-            ] ,$this->votes);
+        return 
+            array_map(
+                fn($vote) => [
+                    $vote['id'],
+                    $vote['vote_text'],
+                    $vote['number_of_votes'] ?: '0', // No votes received yet
+                ], 
+                $this->votes
+            );
     }
 
     public function headings(): array
--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,6 +1,14 @@
 @component('mail::message')
 {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
+@component
+<img src="https://quickchart.io/chart/render/zm-4bab0620-25fd-4265-a098-ca366fc2c2e2?title=
+         {{ $questionText }}
+         ?&labels={{ implode(',', array_map(fn($r) => $r['vote_text'], $voteResults)) }}
+         &data1={{ implode(',', array_map(fn($r) => $r['number_of_votes'], $voteResults)) }}"
+/>
+@endcomponent
+
 @component('mail::table')
 | #     | VOTE TEXT | # OF VOTES |
 | :---- | :-------- | :--------: |
