2e34d174bf05aa39a3369dd581f51b43634d2a30
peterhrtobar <peter.hrobar@gmail.com>
2024-03-01 20:34:00 +0100

    Finalizing mail template

--- resources/views/emails/votes/results.blade.php
+++ resources/views/emails/votes/results.blade.php
@@ -1,5 +1,6 @@
 @component('mail::message')
-{{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
+
+# {{ __('You have been receiving the voting results for _":questionText"_ question!', ['questionText' => $questionText]) }}
 
 <img src="https://quickchart.io/chart/render/zm-710fe8ea-3310-45af-807e-e10634eb78b7?title=
     {{ urlencode($questionText) }}
@@ -15,18 +16,24 @@
     &data1={{ implode(',', array_map(fn($r) => $r['number_of_votes'], $voteResults)) }}"
 />
 
+@component('mail::panel')
+{{ __('Please find each choice with the corresponding number of votes received in the table below.') }}
+@endcomponent
+
 @component('mail::table')
-| #     | VOTE TEXT | # OF VOTES |
+| #     | Choice    | # of votes |
 | :---- | :-------- | :--------: |
 @foreach($voteResults as $result)
 | {{ $result['id'] }} | {{ $result['vote_text'] }} | {{ $result['number_of_votes'] }} |
 @endforeach
 @endcomponent
 
+@component('mail::panel')
 {{ __('The following table includes the number of votes received by location based on your voters geographical data.') }}
+@endcomponent
 
 @component('mail::table')
-| COUNTRY   | CITY | # OF VOTES | LOCATION |
+| Country   | City | # of votes | Location |
 | :-------- | :--- | :--------: | :------: |
 @foreach($voteLocations as $location)
 | {{ $location['country_name'] }} | {{ $location['city'] }} | {{ $location['vote_count'] }} | <a href="https://www.google.com/maps/search/?api=1&query={{ $location['latitude'] }},{{ $location['longitude'] }}" target="_blank" rel="noopener noreferrer"><img src="{{ $message->embed(asset('storage/' . 'images/pin.jpeg')) }}" width="32" height="32" alt="{{ __('Display on Google Maps') }}" /></a> |
