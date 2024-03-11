1de631196ea1eb83dd092d0fe73a4f1eae7a78a8
peterhrtobar <peter.hrobar@gmail.com>
2024-03-11 16:16:12 +0100

    Bug fixing and performance improvements

--- resources/views/list-results.blade.php
+++ resources/views/list-results.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex space-x-2 items-center">
-            <a href="{{ route('questions') }}" title=" {{ __('Back to questions') }}">
-                <i class="fa-solid fa-arrow-left text-2xl text-gray-400 dark:text-gray-200"></i>
+            <a href="{{ route('questions') }}" title=" {{ __('Back to questions') }}" class="inline-block relative group">
+                <div class="rounded-full w-12 h-12 flex items-center justify-center p-2 group-hover:bg-gray-200 group-hover:dark:bg-gray-600">
+                    <i class="fa-solid fa-arrow-left text-2xl text-gray-400 dark:text-gray-200"></i>
+                </div>
             </a>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 {{ __('Show voting results') }}
